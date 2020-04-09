afe1fe55e844283f3b908007a4c1f5a57c7884e0
Danylo Dudok <ddudo@softserveinc.com>
2020-04-10 00:40:12 +0300

    made project under django

---
+++ taskrunner/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.apps import apps
+from .models import *
+
+admin.site.register(Product)
+admin.site.register(ProductBrand)
+admin.site.register(Manufacturer)
